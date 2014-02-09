0dafa26b19ba01d663accc73074dc7b67f4c932c
mhecke1 <mhecke1@bulldogs.mgccc.edu>
2014-02-09 17:22:31 -0600

    Added royalty no attribution reguired picture to index page

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 	<div id="main">
 		<div id="content">
 			<h2>Welcome to my recipe site</h2>
+			<img src="images/Ramenbowl1.jpg" alt="Bowl of Ramen" height="367px" width="500px">
 			<p>Here I will show you some of my favorite recipes that I have tried over the years.</p>
 			<p>I have plans to add pictures and amounts for these recipes but I will have to wait till I get a camera  and the next time I cook them.</p>
 
