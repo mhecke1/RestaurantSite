d58b059a296cc46de42e4b0695a8bf5d15d76946
mhecke1 <mhecke1@bulldogs.mgccc.edu>
2014-02-08 20:25:01 -0600

    Added an others page to the site where I will go over some misc recipes

---
+++ others.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Other recipes</title>
+	<meta charset="UTF-8">
+	<link href='http://fonts.googleapis.com/css?family=Hammersmith+One' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="wrapper">
+	<div id="header">
+		<h1>Matt's Recipe Site</h1>
+	</div>
+	<?php
+		@include_once("navbar.php");
+	?>
+	
+	<div id="main">
+		<div id="content">
+			<h2></h2>
+			<p>I plan to occupy this section with recipes that either are meant to accompany the main recipes I showcase on the site or
+				recipes that do not warrant their own space</p>
+
+			<h3>Pickled ramen eggs</h3>
+			<p>These usually accompany all bowls at any ramen joint worth its salt but they are delicious on their own. These are based on the best version of them I have had so far.</p>
+			<p>A lot of people will say that to get the perfect soft boiled eggs you need to pay close attention to the time and temperature and this is true but one of the more important things is that you need to have a bowl of ice water nearby to stop them from cooking when they are done.</p>
+			<p>Around the 6 minute mark is the generally accepted time for soft boiled eggs. But they have to be somewhat near room temperature when you put them in the boiling water and you have to be sensitive to how much the  eggs are changing the temperature of the water. If you have 6 or 7 cool eggs and you put them in a small pot of boiling water you are actually losing some boiling time on the eggs and you need adjust your total cook time accordingly. Just remeber that they need to be actually boiling for 5 minutes 45 seconds no matter what to even get close to being "soft boiled."</p>
+			<p>With all that being said lets get to the marinade that you are going to put them in</p>
+			<ul>
+				<li>3/4 cup of soy sauce</li>
+				<li>1/4 cup of Kotteri(thick) Mirin</li>
+				<li>teaspoon of honey or sugar</li>
+				<li>1/3 teaspoon of sesame oil</li>
+				<li>1/3 teaspoon of chili (rooster) sauce to give it that bite</li>
+			</ul>
+
+			
+
+		</div>
+		<div id="footer">
+			<p>Copyright Matthew Hecker | matthw@gmail.com</p>
+
+		</div>
+	</div>
+</div>
+	
+	
+</body>
+</html>
