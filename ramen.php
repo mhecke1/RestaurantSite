979c0957588da49ebfd25fe711cea807c72162b8
OpenShift guest <mhecke1@bulldogs.mgccc.edu>
2014-02-15 19:09:16 -0500

    Added a short list of ingredients to ramen page. First commit using cloud 9 IDE

--- ramen.php
+++ ramen.php
@@ -19,6 +19,20 @@
 			<h2>Matts Ramen</h2>
 			<p>This is not your ordinary ramen, This is alot more similar to how it is made in japan. The next time I make it I plan to add the exact amounts and pictures so this is really just a placeholder for now. Basicaly you  roast pork neckbones in an oven as you simmer shitake mushrooms to make dashi for an hour. Then take out the mushrooms, add the porkbones, carrots, scallion trimmings, pan seared garlic and ginger, maybe some mirepoix, and some leeks and let simmer till the sun goes down.</p>
 			<p>Ramen has a different kind of identity in japan than here in the US. To us its at home instant food, in japan its fast food of the gods. prepared for hours in large pots before it is ordered in restaurants that only serve one type of ramen. It basically fastfood with alot of preparation and its really good. </p>
+			<p>I have a Basic outline for the ingredient needed amounts are not                 going to be exact at this point</p>
+			<h4>This is just for the broth</h4>
+			<ul>
+			    <li>2 packs of  pork neckbones</li>
+			    <li>2 packs of dried shiitake mushrooms</li>
+			    <li>Leeks</li>
+			    <li>alot of scallion trimmings</li>
+			    <li>yellow onions</li>
+			    <li>Carrots</li>
+			    <li> fish sauce</li>
+			    <li>garlic</li>
+			    <li>ginger root</li>
+		    </ul>
+			    
 
 		</div>
 		<div id="footer">
